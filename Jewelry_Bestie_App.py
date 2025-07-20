a519a6e77ce1344b41b8d7ecd2af750241e1ec1d
mreporter <mommyreporter@gmail.com>
2025-07-19 21:46:52 -0700

    Update Jewelry_Bestie_App.py

--- Jewelry_Bestie_App.py
+++ Jewelry_Bestie_App.py
@@ -76,7 +76,7 @@ if not st.session_state.clear_fields:
             })
             st.session_state.clear_fields = False
             st.session_state.new_report = False
-            st.experimental_set_query_params(rerun="1")
+            st.query_params = {"rerun": "1"}  # Updated to st.query_params
 
 # Display last report if available
 if st.session_state.report_history:
@@ -94,7 +94,7 @@ if st.session_state.report_history:
         st.session_state.jewelry_type = ""
         st.session_state.user_notes = ""
         st.session_state.session_id = datetime.now().strftime("%Y%m%d%H%M%S")
-        st.experimental_set_query_params(reset="1")
+        st.query_params = {"reset": "1"}  # Updated to st.query_params
 
 # Scroll to top if new session
 if st.session_state.new_report:
